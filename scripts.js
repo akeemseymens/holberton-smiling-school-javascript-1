88c518c3dd03f2620f990be9b5a2d09c193e9091
Brent Muha <brent.muha@gmail.com>
2020-10-08 15:26:38 -0700

    Add courses

--- scripts.js
+++ scripts.js
@@ -106,6 +106,114 @@ function createLatest(data) {
   }
 }
 
+function createDropdowns(data) {
+  $('.topicDefault').html(data.topics[0]);
+  $('.sortDefault').html(data.sorts[0].replace('_', ' '));
+  data.topics.forEach(function (topic) {
+    $('#topicDropdown').append(
+      `<a class="dropdown-item" sectionId="${topic}">${topic}</a>`
+    );
+  });
+  data.sorts.forEach(function (sort) {
+    $('#sortDropdown').append(
+      `<a class="dropdown-item" sectionId="${sort}">${sort.replace(
+        '_',
+        ' '
+      )}</a>`
+    );
+  });
+  $('input').change(function () {
+    displayCourseVideos(
+      $('input').val(),
+      $('.topicDefault').html(),
+      $('.sortDefault').html().replace(' ', '_')
+    );
+  });
+  $('#topicDropdown a').click(function () {
+    $('.topicDefault').html($(this).attr('sectionId'));
+    displayCourseVideos(
+      $('input').val(),
+      $('.topicDefault').html(),
+      $('.sortDefault').html().replace(' ', '_')
+    );
+  });
+  $('#sortDropdown a').click(function () {
+    $('.sortDefault').html($(this).attr('sectionId').replace('_', ' '));
+    displayCourseVideos(
+      $('input').val(),
+      $('.topicDefault').html(),
+      $('.sortDefault').html().replace(' ', '_')
+    );
+  });
+}
+
+function displayCourseVideos(q, topic, sort) {
+  $('.loader').show();
+  $.ajax({
+    type: 'GET',
+    url: 'https://smileschool-api.hbtn.info/courses',
+    dataType: 'json',
+    data: {
+      q,
+      topic,
+      sort,
+    },
+    success: function (response) {
+      if (response.courses.length == 1) {
+        $('#videoCount').html(`1 video`);
+      } else {
+        $('#videoCount').html(`${response.courses.length} videos`);
+      }
+      $('#courseResults').empty();
+      response.courses.forEach(function (course) {
+        $('#courseResults').append(`
+            <div class="col-sm-6 col-md-4 col-lg-3">
+                <div class="card video-card my-3 mx-2">
+                    <img class="card-img-top" src="${course.thumb_url}" alt="Thumbnail" width="255" height="154">
+                    <img class="play-img" src="images/play.png" alt="Play" width="64" height="64">
+                    <div class="card-body">
+                        <p class="font-weight-bold">${course.title}<br>
+                            <span class="text-secondary font-14 font-weight-normal">${course['sub-title']}</span>
+                        </p>
+                        <div class="row justify-content-start font-14 purple-text">
+                            <div class="col-2">
+                                <img class="rounded-circle" src="images/profile_1.jpg" width="30" height="30" alt="Profile 1" loading="lazy">
+                            </div>
+                            <div class="col mt-1">
+                                ${course.author}
+                            </div>
+                        </div>
+                        <div class="row justify-content-between mt-2">
+                            <div class="col" id="stars-course-${course.id}">
+                            </div>
+                            <div class="col-4 text-right purple-text">
+                                ${course.duration}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `);
+        for (let i = 0; i < 5; i++) {
+          if (i < course.star) {
+            $(`#stars-course-${course.id}:nth-child(1)`).append(
+              '<img src="images/star_on.png" width="15" height="15" alt="Star on" loading="lazy">'
+            );
+          } else {
+            $(`#stars-course-${course.id}:nth-child(1)`).append(
+              '<img src="images/star_off.png" width="15" height="15" alt="Star off" loading="lazy">'
+            );
+          }
+        }
+      });
+      $('.loader').hide();
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
+}
+
 function queryQuotes() {
   $('.loader').show();
   $.ajax({
@@ -154,7 +262,6 @@ function queryLatest() {
     url: 'https://smileschool-api.hbtn.info/latest-videos',
     contentType: 'json',
     success: function (response) {
-      console.log(response);
       response.forEach(function (data) {
         createLatest(data);
       });
@@ -169,6 +276,24 @@ function queryLatest() {
   });
 }
 
+function queryCourses() {
+  $('.loader').show();
+  $.ajax({
+    type: 'GET',
+    url: 'https://smileschool-api.hbtn.info/courses',
+    dataType: 'json',
+    success: function (response) {
+      createDropdowns(response);
+      $('.loader').hide();
+    },
+    error: function (error) {
+      console.log(error);
+    },
+  });
+}
+
 queryQuotes();
 queryTutorials();
 queryLatest();
+queryCourses();
+displayCourseVideos();
